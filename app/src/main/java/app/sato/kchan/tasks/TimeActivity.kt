981c0799212f95e7a68436061c25044b338a7220
Kchan-m <atr10180108@gmail.com>
2022-12-22 13:38:02 +0900

    picker実装・変更

--- app/src/main/java/app/sato/kchan/tasks/TimeActivity.kt
+++ app/src/main/java/app/sato/kchan/tasks/TimeActivity.kt
@@ -1,16 +1,30 @@
 package app.sato.kchan.tasks
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import app.sato.kchan.tasks.databinding.TimeActivityBinding
+import java.util.*
 
 class TimeActivity: AppCompatActivity(){
     private lateinit var binding: TimeActivityBinding
+    var setting = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = TimeActivityBinding.inflate(layoutInflater).apply { setContentView(this.root) }
+
+        if (setting) {
+            binding.start.setOnClickListener {
+                showDatePickerDialog()
+            }
+
+            binding.end.setOnClickListener {
+                showDatePickerDialog()
+            }
+        }
 
         val toolbar = binding.toolbar
         setSupportActionBar(toolbar)
@@ -25,5 +39,38 @@ class TimeActivity: AppCompatActivity(){
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    fun showDatePickerDialog() {
+        val calendar: Calendar = Calendar.getInstance()
+
+        //日付ピッカーダイアログを生成および設定
+        DatePickerDialog(
+            this,
+            //ダイアログのクリックイベント設定
+            DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+                val currentDate =
+                    Calendar.getInstance().apply { set(year, monthOfYear, dayOfMonth) }
+
+                showTimePickerDialog()
+            },
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        ).apply {
+        }.show()
+    }
+
+    fun showTimePickerDialog() {
+        val calendar: Calendar = Calendar.getInstance()
+
+        val cal = Calendar.getInstance()
+        val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
+            cal.set(Calendar.HOUR_OF_DAY, hour)
+            cal.set(Calendar.MINUTE, minute)
+        }
+
+        //タイムピッカーダイアログを生成および設定
+        TimePickerDialog(this, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
     }
 }
