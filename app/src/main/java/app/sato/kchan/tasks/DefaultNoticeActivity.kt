ed3617f728163cc3eefd732bc1fd7f3d0c111e81
Kchan-m <atr10180108@gmail.com>
2022-12-26 19:05:46 +0900

    map実装など

--- app/src/main/java/app/sato/kchan/tasks/DefaultNoticeActivity.kt
+++ app/src/main/java/app/sato/kchan/tasks/DefaultNoticeActivity.kt
@@ -1,9 +1,11 @@
 package app.sato.kchan.tasks
 
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import app.sato.kchan.tasks.databinding.DefaultNoticeActivityBinding
+import java.util.*
 
 class DefaultNoticeActivity: AppCompatActivity(){
     private lateinit var binding: DefaultNoticeActivityBinding
@@ -11,6 +13,10 @@ class DefaultNoticeActivity: AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DefaultNoticeActivityBinding.inflate(layoutInflater).apply { setContentView(this.root) }
+
+        binding.editTextTime.setOnClickListener {
+            showTimePickerDialog()
+        }
 
         val toolbar = binding.toolbar
         setSupportActionBar(toolbar)
@@ -25,5 +31,18 @@ class DefaultNoticeActivity: AppCompatActivity(){
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    fun showTimePickerDialog() {
+        val calendar: Calendar = Calendar.getInstance()
+
+        val cal = Calendar.getInstance()
+        val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
+            cal.set(Calendar.HOUR_OF_DAY, hour)
+            cal.set(Calendar.MINUTE, minute)
+        }
+
+        //タイムピッカーダイアログを生成および設定
+        TimePickerDialog(this, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
     }
 }
--- app/src/main/java/app/sato/kchan/tasks/LocationActivity.kt
+++ app/src/main/java/app/sato/kchan/tasks/LocationActivity.kt
@@ -1,9 +1,21 @@
 package app.sato.kchan.tasks
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import app.sato.kchan.tasks.databinding.LocationActivityBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
 
 class LocationActivity: AppCompatActivity(){
     private lateinit var binding: LocationActivityBinding
@@ -11,6 +23,30 @@ class LocationActivity: AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = LocationActivityBinding.inflate(layoutInflater).apply { setContentView(this.root) }
+
+        var location_list = listOf("未選択", "Mapを表示") // 仮置き
+        val adapter = ArrayAdapter(this, R.layout.spinner, location_list)
+
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown)
+        binding.locationSpinner.adapter = adapter
+        binding.locationSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
+                if (pos == 1) {
+                    binding.mapButton.isVisible = true
+                } else {
+                    // 場所設定
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+        }
+
+        binding.mapButton.setOnClickListener {
+            val map = Intent(this, MapsActivity::class.java)
+            startActivity(map)
+        }
 
         val toolbar = binding.toolbar
         setSupportActionBar(toolbar)
@@ -26,4 +62,5 @@ class LocationActivity: AppCompatActivity(){
         }
         return super.onOptionsItemSelected(item)
     }
+
 }
